98978567ce23cd062549cd98d3a27334f1d8b457
Elias <mail@eliaszerrouq.nl>
2014-12-30 13:00:10 +0000

    #82: add ListCommand with vendor filter. Add test for ListCommand

---
+++ src/N98/Magento/Command/Developer/Module/ListCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace N98\Magento\Command\Developer\Module;
+
+use N98\Magento\Command\AbstractMagentoCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
+
+class ListCommand extends AbstractMagentoCommand
+{
+    protected $moduleList = [];
+
+    public function getModuleList()
+    {
+        return $this->moduleList;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('dev:module:list')
+            ->addOption('vendor', null, InputOption::VALUE_OPTIONAL, 'Show modules of a specific vendor (case insensitive)')
+            ->setDescription('List all installed modules')
+            ->addOption(
+                'format',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
+            )
+        ;
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->detectMagento($output, true);
+
+        if ($input->getOption('format') == null) {
+            $this->writeSection($output, 'Magento System Information');
+        }
+
+        $this->initMagento();
+        $this->prepareModuleList($input->getOption('vendor'));
+
+        $this->getHelper('table')
+            ->setHeaders(array('Name', '(Schema) Version'))
+            ->renderByFormat($output, $this->moduleList, $input->getOption('format'));
+
+    }
+
+    protected function prepareModuleList($vendor)
+    {
+        foreach ($this->getObjectManager()->get('\Magento\Framework\Module\ModuleListInterface')->getAll() as $moduleName => $info) {
+            // First index is (probably always) vendor
+            $moduleNameData = explode('_', $moduleName);
+
+            if (! is_null($vendor) && strtolower($moduleNameData[0]) !== strtolower($vendor)) {
+                continue;
+            }
+
+            $this->moduleList[] = [$info['name'], $info['schema_version']];
+        }
+    }
+}
