e261f0e9c9eadf3508e41550ef65629a7d9f5a00
Christian Münch <c.muench@netz98.de>
2015-01-26 17:57:34 +0100

    [TASK] Added sys:setup:compare-versions command.

--- src/N98/Magento/Command/System/Setup/CompareVersionsCommand.php
+++ src/N98/Magento/Command/System/Setup/CompareVersionsCommand.php
@@ -11,6 +11,20 @@ use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
 
 class CompareVersionsCommand extends AbstractMagentoCommand
 {
+    /**
+     * @var \Magento\Framework\Module\ResourceInterface
+     */
+    protected $moduleResource;
+
+    /**
+     * @var \Magento\Framework\Module\ModuleListInterface
+     */
+    protected $moduleList;
+
+    /**
+     * @var \Magento\Framework\Module\ResourceResolverInterface
+     */
+    protected $resourceResolver;
 
     protected function configure()
     {
@@ -32,33 +46,43 @@ HELP;
     }
 
     /**
+     * @param \Magento\Framework\Module\ModuleListInterface $moduleList
+     * @param \Magento\Framework\Module\ResourceResolverInterface $resourceResolver
+     * @param \Magento\Framework\Module\ResourceInterface $moduleResource
+     */
+    public function inject(
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        \Magento\Framework\Module\ResourceResolverInterface $resourceResolver,
+        \Magento\Framework\Module\ResourceInterface $moduleResource
+    ) {
+        $this->moduleList = $moduleList;
+        $this->resourceResolver = $resourceResolver;
+        $this->moduleResource = $moduleResource;
+    }
+
+    /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      * @return int|null|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->detectMagento($output);
-        if ($this->initMagento()) {
-            $time = microtime(true);
-            $modules            = \Mage::getConfig()->getNode('modules');
-            $resourceModel      = $this->_getResourceSingleton('core/resource', 'Mage_Core_Model_Resource_Resource');
-            $setups             = \Mage::getConfig()->getNode('global/resources')->children();
-            $ignoreDataUpdate   = $input->getOption('ignore-data');
+        $time = microtime(true);
+        $ignoreDataUpdate   = $input->getOption('ignore-data');
 
-            $headers = array('Setup', 'Module', 'DB', 'Data', 'Status');
-            if ($ignoreDataUpdate) {
-                unset($headers[array_search('Data', $headers)]);
-            }
+        $headers = array('Setup', 'Module', 'DB', 'Data', 'Status');
+        if ($ignoreDataUpdate) {
+            unset($headers[array_search('Data', $headers)]);
+        }
 
-            $errorCounter = 0;
-            $table = array();
-            foreach ($setups as $setupName => $setup) {
-                $moduleName     = (string) $setup->setup->module;
-                $moduleVersion  = (string) $modules->{$moduleName}->version;
-                $dbVersion      = (string) $resourceModel->getDbVersion($setupName);
+        $errorCounter = 0;
+        $table = array();
+        foreach ($this->moduleList->getAll() as $moduleName => $moduleInfo) {
+            foreach ($this->resourceResolver->getResourceList($moduleName) as $resourceName) {
+                $moduleVersion  = $moduleInfo['schema_version'];
+                $dbVersion      = $this->moduleResource->getDbVersion($resourceName);
                 if (!$ignoreDataUpdate) {
-                    $dataVersion = (string) $resourceModel->getDataVersion($setupName);
+                    $dataVersion = $this->moduleResource->getDataVersion($resourceName);
                 }
                 $ok = $dbVersion == $moduleVersion;
                 if ($ok && !$ignoreDataUpdate) {
@@ -69,7 +93,7 @@ HELP;
                 }
 
                 $row = array(
-                    'Setup'     => $setupName,
+                    'Setup'     => $resourceName,
                     'Module'    => $moduleVersion,
                     'DB'        => $dbVersion,
                 );
@@ -80,52 +104,52 @@ HELP;
                 $row['Status'] = $ok ? 'OK' : 'Error';
                 $table[] = $row;
             }
+        }
 
-            //if there is no output format
-            //highlight the status
-            //and show error'd rows at bottom
+        //if there is no output format
+        //highlight the status
+        //and show error'd rows at bottom
+        if (!$input->getOption('format')) {
+
+            usort($table, function($a, $b) {
+                return $a['Status'] !== 'OK';
+            });
+
+            array_walk($table, function (&$row) {
+                $status             = $row['Status'];
+                $availableStatus    = array('OK' => 'info', 'Error' => 'error');
+                $statusString       = sprintf(
+                    '<%s>%s</%s>',
+                    $availableStatus[$status],
+                    $status,
+                    $availableStatus[$status]
+                );
+                $row['Status'] = $statusString;
+            });
+        }
+
+        if ($input->getOption('log-junit')) {
+            $this->logJUnit($table, $input->getOption('log-junit'), microtime($time) - $time);
+        } else {
+            $this->getHelper('table')
+                ->setHeaders($headers)
+                ->renderByFormat($output, $table, $input->getOption('format'));
+
+            //if no output format specified - output summary line
             if (!$input->getOption('format')) {
-
-                usort($table, function($a, $b) {
-                    return $a['Status'] !== 'OK';
-                });
-
-                array_walk($table, function (&$row) {
-                    $status             = $row['Status'];
-                    $availableStatus    = array('OK' => 'info', 'Error' => 'error');
-                    $statusString       = sprintf(
-                        '<%s>%s</%s>',
-                        $availableStatus[$status],
-                        $status,
-                        $availableStatus[$status]
+                if ($errorCounter > 0) {
+                    $this->writeSection(
+                        $output,
+                        sprintf(
+                            '%s error%s %s found!',
+                            $errorCounter,
+                            $errorCounter === 1 ? '' : 's',
+                            $errorCounter === 1 ? 'was' : 'were'
+                        ),
+                        'error'
                     );
-                    $row['Status'] = $statusString;
-                });
-            }
-
-            if ($input->getOption('log-junit')) {
-                $this->logJUnit($table, $input->getOption('log-junit'), microtime($time) - $time);
-            } else {
-                $this->getHelper('table')
-                    ->setHeaders($headers)
-                    ->renderByFormat($output, $table, $input->getOption('format'));
-
-                //if no output format specified - output summary line
-                if (!$input->getOption('format')) {
-                    if ($errorCounter > 0) {
-                        $this->writeSection(
-                            $output,
-                            sprintf(
-                                '%s error%s %s found!',
-                                $errorCounter,
-                                $errorCounter === 1 ? '' : 's',
-                                $errorCounter === 1 ? 'was' : 'were'
-                            ),
-                            'error'
-                        );
-                    } else {
-                        $this->writeSection($output, 'No setup problems were found.', 'info');
-                    }
+                } else {
+                    $this->writeSection($output, 'No setup problems were found.', 'info');
                 }
             }
         }
