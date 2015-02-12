c5702e42177b77dc5c19c74a54429af538e1eb79
andreaspenz <testa.peta@gmail.com>
2015-02-12 18:59:38 +0000

    Ported sys:store:config:base-url:list command

---
+++ dev/tests/N98/Magento/Command/System/Store/Config/BaseUrlListCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace N98\Magento\Command\System\Store\Config;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use N98\Magento\Command\PHPUnit\TestCase;
+
+class BaseUrlListCommandTest extends TestCase
+{
+    
+    public function testExecute()
+    {
+        $application = $this->getApplication();
+        $application->add(new BaseUrlListCommand());
+        $command = $this->getApplication()->find('sys:store:config:base-url:list');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        $this->assertRegExp('/id/', $commandTester->getDisplay());
+        $this->assertRegExp('/code/', $commandTester->getDisplay());
+        $this->assertRegExp('/unsecure_baseurl/', $commandTester->getDisplay());
+        $this->assertRegExp('/secure_baseurl/', $commandTester->getDisplay());
+    }
+}
