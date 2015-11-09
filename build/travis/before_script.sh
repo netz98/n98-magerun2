659013651116ffc129e08c1f682ff06969e30860
Tom Klingenberg <tklingenberg@lastflood.net>
2015-11-10 00:52:05 +0100

    [TASK] add build on 5.5 and 5.6 and PHP 7 lint
    add build.sh script for straight forward phar-file-builds like in the
    magento 1 repository. to streamline the build with magneto 1, the
    following changes have been made:
    
    - add build script
    
    - put build requirements under configuration management
    
    - read project name from build.xml
    
    - streamline build.sh with magento 1 project
    
    - streamline build.xml with magento 1 project

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-# Install MySQL 5.6
-sudo apt-get remove --purge mysql-common mysql-server-5.5 mysql-server-core-5.5 mysql-client-5.5 mysql-client-core-5.5;
-sudo apt-get autoremove;
-sudo apt-get autoclean;
-sudo apt-add-repository ppa:ondrej/mysql-5.6 -y;
-sudo apt-get update;
-sudo apt-get install mysql-server-5.6 mysql-client-5.6;
-mysql -uroot -e 'SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION; CREATE DATABASE magento_travis;';
+# only install magento if MAGENTO_VERSION has been set
 
-# Install Magento with install command
-bin/n98-magerun2 install --magentoVersionByName="${MAGENTO_VERSION}" --installationFolder="./${MAGENTO_VERSION}" --dbHost=localhost --dbUser=root --dbPass='' --dbName="magento_travis" --installSampleData=${INSTALL_SAMPLE_DATA} --useDefaultConfigParams=yes --baseUrl="http://travis.magento.local/"
+if [ ! -z ${MAGENTO_VERSION+x} ]; then
 
-# Prepare test framework
-export N98_MAGERUN2_TEST_MAGENTO_ROOT="./${MAGENTO_VERSION}"
+    # Install MySQL 5.6
+    ./build/travis/mysql-5.6-install.sh
+
+    echo "installing magento ${MAGENTO_VERSION}"
+
+    export N98_MAGERUN2_TEST_MAGENTO_ROOT="./${MAGENTO_VERSION}"
+
+    bin/n98-magerun2 install \
+        --magentoVersionByName="${MAGENTO_VERSION}" --installationFolder="./${MAGENTO_VERSION}" \
+        --dbHost=localhost --dbUser=root --dbPass='' --dbName="magento_travis" \
+        --installSampleData=${INSTALL_SAMPLE_DATA} --useDefaultConfigParams=yes \
+        --baseUrl="http://travis.magento.local/"
+
+else
+
+    echo "no magento version to install"
+
+fi
---
+++ build/travis/mysql-5.6-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Install MySQL 5.6 on travis
+#
+
+sudo apt-get remove --purge mysql-common mysql-server-5.5 mysql-server-core-5.5 mysql-client-5.5 mysql-client-core-5.5;
+sudo apt-get autoremove;
+sudo apt-get autoclean;
+sudo apt-add-repository ppa:ondrej/mysql-5.6 -y;
+sudo apt-get update;
+sudo apt-get install mysql-server-5.6 mysql-client-5.6;
+mysql -uroot -e 'SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION; CREATE DATABASE magento_travis;';
