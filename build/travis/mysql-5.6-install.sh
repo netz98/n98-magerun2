7edbe184d8ad3e10a7487cc3fc7a4c8c6f59346f
Tom Klingenberg <tklingenberg@lastflood.net>
2016-01-03 22:21:38 +0100

    [BUILD] update mysql 5.6 setup on travis
    with the better error checking to fail early, it came to attention that
    the procedure to install mysql 5.6 on travis trusty beta could be sim-
    plified as the package is now directly available.
    
    still ref: 713f40f

--- build/travis/mysql-5.6-install.sh
+++ build/travis/mysql-5.6-install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
+
 #
 # Install MySQL 5.6 on travis
 #
 
-sudo apt-get remove --purge mysql-common mysql-server-5.5 mysql-server-core-5.5 mysql-client-5.5 mysql-client-core-5.5;
-sudo apt-get autoremove;
-sudo apt-get autoclean;
-sudo apt-add-repository ppa:ondrej/mysql-5.6 -y;
 sudo apt-get update -qq;
 sudo apt-get install -qq mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6;
 mysql -uroot -e 'SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION; CREATE DATABASE magento_travis;';
