3c92fb4f858410453aad5e08fc338975d00e4eac
Christian Münch <c.muench@netz98.de>
2016-04-08 14:33:22 -0700

    [TASK] Refactored some code and add tree command.

---
+++ src/N98/Magento/Command/Developer/Console/Structure/ModuleNameStructure.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © 2016 netz98 new media GmbH. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace N98\Magento\Command\Developer\Console\Structure;
+
+class ModuleStructure
+{
+    /**
+     * @var string
+     */
+    private $vendorName;
+
+    /**
+     * @var string
+     */
+    private $shortModuleName;
+
+    /**
+     * @param string $fullModuleName like Acme_Foo
+     */
+    public function __construct($fullModuleName)
+    {
+        $parts = explode('_', $fullModuleName);
+
+        if (count($parts) !== 2) {
+            throw new \InvalidArgumentException('Please specify a correct module name like Acme_Foo');
+        }
+
+        $this->vendorName = ucfirst($parts[0]);
+        $this->shortModuleName = ucfirst($parts[1]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getShortModuleName()
+    {
+        return $this->shortModuleName;
+    }
+
+    /**
+     * Returns the full module name in style Acme_Foo
+     *
+     * @return string
+     */
+    public function getFullModuleName()
+    {
+        return $this->vendorName . '_' . $this->shortModuleName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVendorName()
+    {
+        return $this->vendorName;
+    }
+}
---
+++ src/N98/Magento/Command/Developer/Console/Structure/ThemeNameStructure.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © 2016 netz98 new media GmbH. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace N98\Magento\Command\Developer\Console\Structure;
+
+class ThemeNameStructure
+{
+
+}
