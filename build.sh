95b0c1b4f786ab770c65871eb9b68ecb13207358
Tom Klingenberg <tklingenberg@lastflood.net>
2016-05-05 16:39:29 +0200

    Make build script exit early
    Make build.sh exit early by updating to bash unofficial strict mode
    defaults and exiting early if phing build fails.
    
    This should make failure in the build process more clear and reduce
    debugging efforts.
    
    Additionallyi some other changes are made to foster stability of the
    build script:
    
    - Show composer version in build script
    
    - Fix typo
    
    - Clean phar file before build

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # build from clean checkout
 #
+set -euo pipefail
+IFS=$'\n\t'
 
 name="$(awk '/<project name="([^"]*)"/ && !done {print gensub(/<project name="([^"]*)".*/, "\\1", "g"); done=1}' build.xml
 )"
 phar="${name}.phar"
 
-echo "building ${phar}..."
+echo "Building ${phar}..."
 
 if [ ! -e "composer.phar" ]; then
+    echo "Downloading composer.phar..."
     wget http://getcomposer.org/composer.phar
     chmod +x composer.phar
 fi
+
+./composer.phar --version
 
 if [ ! -d "vendor" ]; then
     ./composer.phar install --no-interaction
@@ -22,9 +27,21 @@ if [ ! -d "build/vendor" ]; then
     ./composer.phar -d=build install --no-interaction
 fi
 
+if [ -e "${phar}" ]; then
+    echo "Remove earlier created ${phar} file"
+    rm "${phar}"
+fi
+
 ulimit -Sn $(ulimit -Hn)
+
+set +e
 php -f build/vendor/phing/phing/bin/phing -dphar.readonly=0 -- -verbose dist
 BUILD_STATUS=$?
+set -e
+if [ ${BUILD_STATUS} -ne 0 ]; then
+    >&2 echo "error: phing build failed with exit status ${BUILD_STATUS}"
+    exit ${BUILD_STATUS}
+fi
 
 php -f "${phar}" -- --version
 
@@ -32,7 +49,3 @@ ls -l "${phar}"
 
 php -r 'echo "SHA1: ", sha1_file("'"${phar}"'"), "\nMD5.: ", md5_file("'"${phar}"'"), "\n";'
 
-if [ ${BUILD_STATUS} -ne 0 ]; then
-    >&2 echo "error: phing build failed with exit status ${BUILD_STATUS}"
-    exit ${BUILD_STATUS}
-fi
