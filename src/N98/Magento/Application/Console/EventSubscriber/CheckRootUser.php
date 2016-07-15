78b77a37991c9b6a054d6be6412d05ed67defd9b
Tom Klingenberg <tklingenberg@lastflood.net>
2016-07-16 00:20:38 +0200

    Polish

--- src/N98/Magento/Application/Console/EventSubscriber/CheckRootUser.php
+++ src/N98/Magento/Application/Console/EventSubscriber/CheckRootUser.php
@@ -9,7 +9,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class CheckRootUser implements EventSubscriberInterface
 {
-    const WARNING_ROOT_USER = '<error>It\'s not recommended to run n98-magerun as root user</error>';
+    const WARNING_ROOT_USER = "<error>It's not recommended to run n98-magerun as root user</error>";
 
     /**
      * Returns an array of event names this subscriber wants to listen to.
@@ -21,7 +21,7 @@ class CheckRootUser implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            Events::RUN_BEFORE     => 'checkRunningAsRootUser'
+            Events::RUN_BEFORE => 'checkRunningAsRootUser'
         ];
     }
 
@@ -42,15 +42,12 @@ class CheckRootUser implements EventSubscriberInterface
             return;
         }
 
-        $output = $event->getOutput();
-        if (OperatingSystem::isLinux() || OperatingSystem::isMacOs()) {
-            if (function_exists('posix_getuid')) {
-                if (posix_getuid() === 0) {
-                    $output->writeln('');
-                    $output->writeln(self::WARNING_ROOT_USER);
-                    $output->writeln('');
-                }
-            }
+        // display if current user is root
+        if (function_exists('posix_getuid') && posix_getuid() === 0) {
+            $output = $event->getOutput();
+            $output->writeln('');
+            $output->writeln(self::WARNING_ROOT_USER);
+            $output->writeln('');
         }
     }
 
