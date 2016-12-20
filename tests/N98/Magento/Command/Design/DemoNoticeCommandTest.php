2134af25e9f6f59825b1a9e6e75913c7b5ba70c5
Jürgen Thelen <j.thelen@netz98.de>
2016-12-20 08:20:15 +0100

    Port of design:demo-notice command, tasks #69

---
+++ tests/N98/Magento/Command/Design/DemoNoticeCommandTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace N98\Magento\Command\Design;
+
+use N98\Magento\Command\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class DemoNoticeCommandTest extends TestCase
+{
+    public function testExecute()
+    {
+        $application = $this->getApplication();
+        $application->add(new DemoNoticeCommand());
+        $application->setAutoExit(false);
+        $command = $this->getApplication()->find('design:demo-notice');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            array(
+                'command'  => $command->getName(),
+                '--global' => true,
+                '--on'     => true,
+            )
+        );
+        $this->assertRegExp('/Demo Notice enabled/', $commandTester->getDisplay());
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            array(
+                'command'  => $command->getName(),
+                '--global' => true,
+                '--off'    => true,
+            )
+        );
+
+        $this->assertRegExp('/Demo Notice disabled/', $commandTester->getDisplay());
+    }
+}
