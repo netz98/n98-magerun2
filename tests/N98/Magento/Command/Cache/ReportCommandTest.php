2f0d46c93473bdfe24df302a40be3f0318dec3d9
Jürgen Thelen <j.thelen@netz98.de>
2017-03-01 20:34:47 +0100

    Porting command cache:report, tasks #39

---
+++ tests/N98/Magento/Command/Cache/ReportCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace N98\Magento\Command\Cache;
+
+use N98\Magento\Command\TestCase;
+
+class ReportCommandTest extends TestCase
+{
+    public function testExecute()
+    {
+        $this->assertDisplayRegExp(
+            array(
+                'command' => 'cache:report',
+            ),
+            '~\| ID.*\| EXPIRE.*\|$\R~m'
+        );
+    }
+}
