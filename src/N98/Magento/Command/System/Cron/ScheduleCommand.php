190b2d435e0e300a602803fbafbb938bd539c88d
Pieter Hoste <hoste.pieter@gmail.com>
2017-03-13 20:31:26 +0100

    Importing Area class.

--- src/N98/Magento/Command/System/Cron/ScheduleCommand.php
+++ src/N98/Magento/Command/System/Cron/ScheduleCommand.php
@@ -3,6 +3,7 @@
 namespace N98\Magento\Command\System\Cron;
 
 use Magento\Cron\Model\Schedule;
+use Magento\Framework\App\Area;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
