b7f0ccbf7d80f8a103344b33e498c2adf40138bf
Christian Münch <c.muench@netz98.de>
2017-03-17 12:56:36 +0100

    [FEATURE] Add new config:data:acl command to print the ACL tree
    The new command prints all ACLs of the system in a tree.

---
+++ src/N98/Magento/Command/Config/Data/AclCommand.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace N98\Magento\Command\Config\Data;
+
+use Magento\Framework\Acl\AclResource\Config\Reader\Filesystem as AclConfigReader;
+use N98\Magento\Command\AbstractMagentoCommand;
+use PBergman\Console\Helper\TreeHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AclCommand extends AbstractMagentoCommand
+{
+    /**
+     * @var AclConfigReader
+     */
+    private $configReader;
+
+    protected function configure()
+    {
+        $this
+            ->setName('config:data:acl')
+            ->setDescription('Prints acl.xml data as table')
+        ;
+    }
+
+    /**
+     * @param AclConfigReader $configReader
+     */
+    public function inject(AclConfigReader $configReader)
+    {
+        $this->configReader = $configReader;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $data = $this->configReader->read();
+
+        $tree = new TreeHelper();
+        $tree->setTitle('ACL Tree');
+
+        $this->recursiveSort($data['config']['acl']['resources']);
+
+        foreach ($data['config']['acl']['resources'] as $row) {
+            $this->renderNode($tree, $row);
+        }
+
+        $tree->printTree($output);
+    }
+
+    /**
+     * @param TreeHelper $tree
+     * @param array $row
+     */
+    protected function renderNode($tree, $row)
+    {
+        $tree = $tree->newNode($this->formatNode($row));
+
+        foreach ($row['children'] as $child) {
+            if (count($child['children']) > 0) {
+                $this->renderNode($tree, $child);
+            } else {
+                $tree->addValue($this->formatNode($child));
+            }
+        }
+
+        $tree->end();
+    }
+
+    /**
+     * @param $row
+     * @return string
+     */
+    private function formatNode($row)
+    {
+        return sprintf('%d: <info>%s</info> [<comment>%s</comment>]', $row['sortOrder'], $row['title'], $row['id']);
+    }
+
+    /**
+     * @param $array
+     * @return void
+     */
+    function recursiveSort(array &$array)
+    {
+        uasort($array, [$this, 'compareNodes']);
+
+        foreach ($array as &$row) {
+            if (count($row['children']) > 0) {
+                $this->recursiveSort($row['children']);
+            }
+        }
+    }
+
+    /**
+     * @param array $a
+     * @param array $b
+     * @return int
+     */
+    private function compareNodes(array $a, array $b)
+    {
+        if ($a['sortOrder'] > $b['sortOrder']) {
+            return 1;
+        }
+        if ($a['sortOrder'] < $b['sortOrder']) {
+            return -1;
+        }
+
+        return 0;
+    }
+}
---
+++ tests/N98/Magento/Command/Config/Data/AclCommandTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace N98\Magento\Command\Config\Data;
+
+use N98\Magento\Command\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class AclCommandTest extends TestCase
+{
+    /**
+     * @test
+     * @outputBuffering off
+     */
+    public function itShouldLoadGlobalConfig()
+    {
+        $command = $this->getApplication()->find('config:data:acl');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            array(
+                'command' => 'config:data:acl',
+            )
+        );
+
+        $this->assertContains('All Stores', $commandTester->getDisplay());
+        $this->assertContains('Magento_Reports::salesroot_sales', $commandTester->getDisplay());
+    }
+}
