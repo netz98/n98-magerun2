85b56fa69420782fa2ea512b6c6ceff3188401ea
Christian Münch <c.muench@netz98.de>
2017-03-17 13:04:50 +0100

    [FEATURE][WIP] New search-engline:list command

---
+++ src/N98/Magento/Command/SearchEngine/ListCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace N98\Magento\Command\SearchEngine;
+
+use N98\Magento\Command\AbstractMagentoCommand;
+use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ListCommand extends AbstractMagentoCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('search-engine:list')
+            ->setDescription('Lists all registered search engines')
+            ->addOption(
+                'format',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
+            )
+        ;
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->detectMagento($output, true);
+        if (!$this->initMagento()) {
+            return;
+        }
+
+        $userList = $this->userModel->getCollection();
+        $table = array();
+        foreach ($userList as $user) {
+            $table[] = array(
+                $user->getId(),
+                $user->getUsername(),
+                $user->getEmail(),
+                $user->getIsActive() ? 'active' : 'inactive',
+            );
+        }
+        $this->getHelper('table')
+            ->setHeaders(array('id', 'username', 'email', 'status'))
+            ->renderByFormat($output, $table, $input->getOption('format'));
+    }
+}
