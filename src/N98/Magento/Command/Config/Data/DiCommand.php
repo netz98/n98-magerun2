5f6a888fb2b67375419d35e4dfe005147edf3fd0
Christian Münch <c.muench@netz98.de>
2017-03-17 18:04:54 +0100

    Fix code style of new di command

--- src/N98/Magento/Command/Config/Data/DiCommand.php
+++ src/N98/Magento/Command/Config/Data/DiCommand.php
@@ -30,8 +30,9 @@ class DiCommand extends AbstractMagentoCommand
     }
 
     /**
-     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
      * @return int|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
