de7449b30f23b9b2047456b3add76c570fcd9bc3
Christian Münch <c.muench@netz98.de>
2017-03-20 13:39:45 +0100

    Remove abstract test class

--- tests/N98/Magento/Command/Developer/Asset/ClearCommandTest.php
+++ tests/N98/Magento/Command/Developer/Asset/ClearCommandTest.php
@@ -2,11 +2,45 @@
 
 namespace N98\Magento\Command\Developer\Asset;
 
+use Magento\Deploy\Model\Mode;
+use Magento\Framework\App\State;
 use Magento\Framework\App\View\Asset\Publisher as AssetPublisher;
 use Magento\Framework\View\Asset\Repository as AssetRepo;
+use N98\Magento\Command\TestCase as BaseTestCase;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
-class ClearCommandTest extends TestCase
+class ClearCommandTest extends BaseTestCase
 {
+    /**
+     * @return void
+     */
+    protected function setUp()
+    {
+        if ($this->runsInProductionMode()) {
+            $this->markTestSkipped('This command is not available in production mode');
+        }
+
+        parent::setUp();
+    }
+
+    /**
+     * @return bool
+     */
+    private function runsInProductionMode()
+    {
+        $objectManager = $this->getApplication()->getObjectManager();
+        $mode = $objectManager->create(
+            Mode::class,
+            [
+                'input'  => new ArgvInput(),
+                'output' => new ConsoleOutput(),
+            ]
+        );
+
+        return $mode->getMode() === State::MODE_PRODUCTION;
+    }
+
     /**
      * @return void
      */
