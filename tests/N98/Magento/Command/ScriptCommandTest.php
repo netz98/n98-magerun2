94608596b3dfd234bfb0e196c439d60e42fd9512
Christian Münch <c.muench@netz98.de>
2017-10-09 20:44:29 +0200

    Updated pre-commit framework to latest version

--- tests/N98/Magento/Command/ScriptCommandTest.php
+++ tests/N98/Magento/Command/ScriptCommandTest.php
@@ -2,6 +2,11 @@
 
 namespace N98\Magento\Command;
 
+/**
+ * Class ScriptCommandTest
+ * @package N98\Magento\Command
+ * @group exclude-2.2
+ */
 class ScriptCommandTest extends TestCase
 {
     public function testExecute()
