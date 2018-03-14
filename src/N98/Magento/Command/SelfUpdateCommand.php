79dd1378f0f3cc31271b36dd1b78ea36af3f025c
Christian Münch <c.muench@netz98.de>
2018-03-14 15:27:53 +0100

    Add --dry-run mode to self-update command

--- src/N98/Magento/Command/SelfUpdateCommand.php
+++ src/N98/Magento/Command/SelfUpdateCommand.php
@@ -16,19 +16,27 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class SelfUpdateCommand extends AbstractMagentoCommand
 {
+    const VERSION_TXT_URL_UNSTABLE = 'https://raw.githubusercontent.com/netz98/n98-magerun2/develop/version.txt';
+    const MAGERUN_DOWNLOAD_URL_UNSTABLE = 'https://files.magerun.net/n98-magerun2-dev.phar';
+    const VERSION_TXT_URL_STABLE = 'https://raw.githubusercontent.com/netz98/n98-magerun2/master/version.txt';
+    const MAGERUN_DOWNLOAD_URL_STABLE = 'https://files.magerun.net/n98-magerun2.phar';
+    const CHANGELOG_DOWNLOAD_URL_UNSTABLE = 'https://raw.github.com/netz98/n98-magerun2/develop/CHANGELOG.md';
+    const CHANGELOG_DOWNLOAD_URL_STABLE = 'https://raw.github.com/netz98/n98-magerun2/master/CHANGELOG.md';
+
     protected function configure()
     {
         $this
             ->setName('self-update')
             ->setAliases(array('selfupdate'))
             ->addOption('unstable', null, InputOption::VALUE_NONE, 'Load unstable version from develop branch')
-            ->setDescription('Updates n98-magerun.phar to the latest version.')
+            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Tests if there is a new version without any update.')
+            ->setDescription('Updates n98-magerun2.phar to the latest version.')
             ->setHelp(
 <<<EOT
 The <info>self-update</info> command checks github for newer
-versions of n98-magerun and if found, installs the latest.
+versions of n98-magerun2 and if found, installs the latest.
 
-<info>php n98-magerun.phar self-update</info>
+<info>php n98-magerun2.phar self-update</info>
 
 EOT
             )
@@ -43,8 +51,16 @@ EOT
         return $this->getApplication()->isPharMode();
     }
 
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|null|void
+     * @throws \Composer\Downloader\FilesystemException
+     * @throws \Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $isDryRun = $input->getOption('dry-run');
         $localFilename = realpath($_SERVER['argv'][0]) ?: $_SERVER['argv'][0];
         $tempFilename = dirname($localFilename) . '/' . basename($localFilename, '.phar') . '-temp.phar';
 
@@ -67,67 +83,30 @@ EOT
 
         $loadUnstable = $input->getOption('unstable');
         if ($loadUnstable) {
-            $versionTxtUrl = 'https://raw.githubusercontent.com/netz98/n98-magerun2/develop/version.txt';
-            $remoteFilename = 'https://files.magerun.net/n98-magerun2-dev.phar';
+            $versionTxtUrl = self::VERSION_TXT_URL_UNSTABLE;
+            $remoteFilename = self::MAGERUN_DOWNLOAD_URL_UNSTABLE;
         } else {
-            $versionTxtUrl = 'https://raw.githubusercontent.com/netz98/n98-magerun2/master/version.txt';
-            $remoteFilename = 'https://files.magerun.net/n98-magerun2.phar';
+            $versionTxtUrl = self::VERSION_TXT_URL_STABLE;
+            $remoteFilename = self::MAGERUN_DOWNLOAD_URL_STABLE;
         }
 
         $latest = trim($rfs->getContents('raw.githubusercontent.com', $versionTxtUrl, false));
 
-        if ($this->getApplication()->getVersion() !== $latest || $loadUnstable) {
+        if ($this->isOutdatedVersion($latest, $loadUnstable)) {
             $output->writeln(sprintf("Updating to version <info>%s</info>.", $latest));
 
-            $rfs->copy('raw.github.com', $remoteFilename, $tempFilename);
-
-            if (!file_exists($tempFilename)) {
-                $output->writeln('<error>The download of the new n98-magerun version failed for an unexpected reason');
-
-                return 1;
-            }
-
             try {
-                \error_reporting(E_ALL); // supress notices
+                $this->downloadNewVersion($output, $rfs, $remoteFilename, $tempFilename);
+                $this->checkNewPharFile($tempFilename, $localFilename);
 
-                @chmod($tempFilename, 0777 & ~umask());
-                // test the phar validity
-                $phar = new \Phar($tempFilename);
-                // free the variable to unlock the file
-                unset($phar);
-                @rename($tempFilename, $localFilename);
+                if (!$isDryRun) {
+                    $this->replaceExistingPharFile($tempFilename, $localFilename);
+                }
+
                 $output->writeln('<info>Successfully updated n98-magerun2</info>');
+                $this->showChangelog($output, $loadUnstable, $rfs);
 
-                if ($loadUnstable) {
-                    $changeLogContent = $rfs->getContents(
-                        'raw.github.com',
-                        'https://raw.github.com/netz98/n98-magerun2/develop/CHANGELOG.md',
-                        false
-                    );
-                } else {
-                    $changeLogContent = $rfs->getContents(
-                        'raw.github.com',
-                        'https://raw.github.com/netz98/n98-magerun2/master/CHANGELOG.md',
-                        false
-                    );
-                }
-
-                if ($changeLogContent) {
-                    $output->writeln($changeLogContent);
-                }
-
-                if ($loadUnstable) {
-                    $unstableFooterMessage = <<<UNSTABLE_FOOTER
-<comment>
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!! DEVELOPMENT VERSION. DO NOT USE IN PRODUCTION !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-</comment>
-UNSTABLE_FOOTER;
-                    $output->writeln($unstableFooterMessage);
-                }
-
-                $this->_exit();
+                $this->_exit(0);
             } catch (\Exception $e) {
                 @unlink($tempFilename);
                 if (!$e instanceof \UnexpectedValueException && !$e instanceof \PharException) {
@@ -137,7 +116,7 @@ UNSTABLE_FOOTER;
                 $output->writeln('<error>Please re-run the self-update command to try again.</error>');
             }
         } else {
-            $output->writeln("<info>You are using the latest n98-magerun version.</info>");
+            $output->writeln("<info>You are using the latest n98-magerun2 version.</info>");
         }
     }
 
@@ -147,10 +126,101 @@ UNSTABLE_FOOTER;
      * This is a workaround to prevent warning of dispatcher after replacing
      * the phar file.
      *
+     * @param int $statusCode
      * @return void
      */
-    protected function _exit()
+    protected function _exit($statusCode = 0)
     {
-        exit;
+        exit($statusCode);
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param $rfs
+     * @param $remoteFilename
+     * @param $tempFilename
+     */
+    private function downloadNewVersion(OutputInterface $output, $rfs, $remoteFilename, $tempFilename)
+    {
+        $rfs->copy('raw.github.com', $remoteFilename, $tempFilename);
+
+        if (!file_exists($tempFilename)) {
+            $output->writeln('<error>The download of the new n98-magerun2 version failed for an unexpected reason');
+        }
+    }
+
+    /**
+     * @param string $tempFilename
+     * @param string $localFilename
+     */
+    private function checkNewPharFile($tempFilename, $localFilename)
+    {
+        \error_reporting(E_ALL); // supress notices
+
+        @chmod($tempFilename, 0777 & ~umask());
+        // test the phar validity
+        $phar = new \Phar($tempFilename);
+        // free the variable to unlock the file
+        unset($phar);
+    }
+
+    /**
+     * @param string $tempFilename
+     * @param string $localFilename
+     */
+    private function replaceExistingPharFile($tempFilename, $localFilename)
+    {
+        if (!@rename($tempFilename, $localFilename)) {
+            throw new \RuntimeException(
+                sprintf('Cannot replace existing phar file "%s". Please check permissions.', $localFilename)
+            );
+        }
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param $loadUnstable
+     * @param $rfs
+     */
+    private function showChangelog(OutputInterface $output, $loadUnstable, $rfs)
+    {
+        if ($loadUnstable) {
+            $changeLogContent = $rfs->getContents(
+                'raw.github.com',
+                self::CHANGELOG_DOWNLOAD_URL_UNSTABLE,
+                false
+            );
+        } else {
+            $changeLogContent = $rfs->getContents(
+                'raw.github.com',
+                self::CHANGELOG_DOWNLOAD_URL_STABLE,
+                false
+            );
+        }
+
+        if ($changeLogContent) {
+            $output->writeln($changeLogContent);
+        }
+
+        if ($loadUnstable) {
+            $unstableFooterMessage = <<<UNSTABLE_FOOTER
+<comment>
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! DEVELOPMENT VERSION. DO NOT USE IN PRODUCTION !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+</comment>
+UNSTABLE_FOOTER;
+            $output->writeln($unstableFooterMessage);
+        }
+    }
+
+    /**
+     * @param $latest
+     * @param $loadUnstable
+     * @return bool
+     */
+    private function isOutdatedVersion($latest, $loadUnstable)
+    {
+        return $this->getApplication()->getVersion() !== $latest || $loadUnstable;
     }
 }
