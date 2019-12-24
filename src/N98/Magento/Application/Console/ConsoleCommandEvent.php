a7901b3e712f61d736eff9945a4000fcbf4de0c4
Christian Münch <christian@muench-worms.de>
2019-12-24 16:31:30 +0100

    Remove old compatibility layer to Symfony Events for older Magento versions
    We support only Magento >= 2.3.
    So we are able to remove this legacy code.

--- src/N98/Magento/Application/Console/ConsoleCommandEvent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace N98\Magento\Application\Console;
-
-class ConsoleCommandEvent extends \Symfony\Component\Console\Event\ConsoleCommandEvent
-{
-    use SymfonyCompatibilityTrait;
-}
