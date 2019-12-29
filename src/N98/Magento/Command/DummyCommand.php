5799c554556f9935d74bff6d0dac9423d053f2cc
Christian Münch <christian@muench-worms.de>
2019-12-29 16:49:14 +0100

    Pass DummyCommand to ConsoleEvent "before"
    This solution is better than loading the wrong ConsoleEvent class. Old Magento 2.2.x installations have a incompatible ConsoleEvent class. The argument  "command" has to be a valid command. In Symfony 3.x the command argument can be null. At this moment we do not have a valid command, because we are before any command execution. So we pass a Dummy Command to the listener.

---
+++ src/N98/Magento/Command/DummyCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace N98\Magento\Command;
+
+/**
+ * Class DummyCommand
+ * @package N98\Magento\Command
+ */
+class DummyCommand extends AbstractMagentoCommand
+{
+    protected function configure()
+    {
+        $this->setName('dummy');
+    }
+
+}
