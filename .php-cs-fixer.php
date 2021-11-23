c30f7f214ff91b9f13b797ff1e9a61fe352c95a8
Christian Münch <christian@muench-worms.de>
2021-11-23 13:53:02 +0100

    Fix php-cs-fixer deprecated warnings

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,11 +1,11 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
+$finder = (new PhpCsFixer\Finder())
     ->in(__DIR__ . '/src')
     ->in(__DIR__ . '/tests')
 ;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setFinder($finder)
     ->setRules([
         '@PSR2' => true,
@@ -14,11 +14,11 @@ return PhpCsFixer\Config::create()
         'include' => true,
         'new_with_braces' => true,
         'no_empty_statement' => true,
-        'no_extra_consecutive_blank_lines' => true,
+        'no_extra_blank_lines' => true,
         'no_leading_import_slash' => true,
         'no_leading_namespace_whitespace' => true,
         'no_multiline_whitespace_around_double_arrow' => true,
-        'no_multiline_whitespace_before_semicolons' => true,
+        'multiline_whitespace_before_semicolons' => false,
         'no_singleline_whitespace_before_semicolons' => true,
         'no_trailing_comma_in_singleline_array' => true,
         'no_unused_imports' => true,
