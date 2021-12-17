e8f65aecfbb3f27d65a88bdfa2fe8b15d08bff0a
Pieter Hoste <hoste.pieter@gmail.com>
2021-12-17 17:52:21 +0100

    Adds a config:env:delete command

--- README.md
+++ README.md
@@ -354,6 +354,24 @@ n98-magerun2.phar config:env:set --input-format=json directories.document_root_i
 n98-magerun2.phar config:env:set --input-format=json cron_consumers_runner.consumers '["some.consumer", "some.other.consumer"]'
 ```
 
+### Delete key from env.php file
+
+Remove a configuration from the env.php file by providing a key.
+
+Sub-arrays in config.php can be specified by adding a "." character to every array.
+
+```sh
+n98-magerun2.phar config:env:delete <key>
+```
+
+Examples:
+
+```sh
+n98-magerun2.phar config:env:delete system
+n98-magerun2.phar config:env:delete cache.frontend.default.backend
+n98-magerun2.phar config:env:delete cache.frontend.default.backend_options
+```
+
 ### Show env.php settings
 
 ```sh
---
+++ tests/N98/Magento/Command/Config/Env/DeleteCommandTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace N98\Magento\Command\Config\Env;
+
+use N98\Magento\Command\TestCase;
+
+/**
+ * Class DeleteCommandTest
+ * @package N98\Magento\Command\Config\Env
+ */
+class DeleteCommandTest extends TestCase
+{
+    public function testExecute()
+    {
+        // first add a dummy key
+        $this->assertExecute(
+            [
+                'command' => 'config:env:set',
+                'key' => 'magerun.test',
+                'value' => 'test'
+            ]
+        );
+
+        // Check if config gets removed
+        $this->assertDisplayContains(
+            [
+                'command' => 'config:env:delete',
+                'key' => 'magerun.test'
+            ],
+            'Config magerun.test successfully removed '
+        );
+
+        // Check for idempotency
+        $this->assertDisplayContains(
+            [
+                'command' => 'config:env:delete',
+                'key' => 'magerun.test',
+                '--verbose' => true // Add dummy option to force different input hash
+            ],
+            'Config doesn\'t exists'
+        );
+    }
+}
