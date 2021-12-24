725ffeae7202570d3ceab2bb101f74ef928ae898
Christian Münch <christian@muench-worms.de>
2021-12-24 08:52:30 +0100

    Change progressbar format in self-update command

--- src/N98/Magento/Command/SelfUpdateCommand.php
+++ src/N98/Magento/Command/SelfUpdateCommand.php
@@ -152,7 +152,7 @@ EOT
     {
         try {
             $progressBar = new ProgressBar($output);
-            $progressBar->setFormat(' %current%/%max% [%bar%] bytes downloaded');
+            $progressBar->setFormat('[%bar%] %current% of %max% bytes downloaded');
 
             $client = new Client([
                 'progress' => function (
