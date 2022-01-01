3cd2e57b5a0fc129aad51421fa6da9b6f25a9621
Christian Münch <christian@muench-worms.de>
2022-01-01 10:40:56 +0100

    Add ddev setup
    Make tests compatible with local ddev setup (MariaDB).
    Exclude .ddev directory in phar build

---
+++ .ddev/bash/helpers/colors.sh
@@ -0,0 +1,54 @@
+################################################################################
+#
+# Terminal Colors
+#
+# See: https://github.com/hootsuite/pre-commit-php/blob/master/pre_commit_hooks/helpers/colors.sh
+#
+################################################################################
+
+# Regular
+txtblk='\033[0;30m' # Black
+txtred='\033[0;31m' # Red
+txtgrn='\033[0;32m' # Green
+txtylw='\033[0;33m' # Yellow
+txtblu='\033[0;34m' # Blue
+txtpur='\033[0;35m' # Purple
+txtcyn='\033[0;36m' # Cyan
+txtwht='\033[0;37m' # White
+
+# Bold
+bldblk='\033[1;30m' # Black
+bldred='\033[1;31m' # Red
+bldgrn='\033[1;32m' # Green
+bldylw='\033[1;33m' # Yellow
+bldblu='\033[1;34m' # Blue
+bldpur='\033[1;35m' # Purple
+bldcyn='\033[1;36m' # Cyan
+bldwht='\033[1;37m' # White
+
+# Underline
+unkblk='\033[4;30m' # Black
+undred='\033[4;31m' # Red
+undgrn='\033[4;32m' # Green
+undylw='\033[4;33m' # Yellow
+undblu='\033[4;34m' # Blue
+undpur='\033[4;35m' # Purple
+undcyn='\033[4;36m' # Cyan
+undwht='\033[4;37m' # White
+
+# Background
+bakblk='\033[40m'   # Black
+bakred='\033[41m'   # Red
+bakgrn='\033[42m'   # Green
+bakylw='\033[43m'   # Yellow
+bakblu='\033[44m'   # Blue
+bakpur='\033[45m'   # Purple
+bakcyn='\033[46m'   # Cyan
+bakwht='\033[47m'   # White
+
+txtrst='\033[0m'    # Text Reset
+
+# Emojii
+check_mark='\xE2\x9C\x94'
+cross_mark='\xE2\x9D\x8C'
+
