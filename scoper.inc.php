d88fdccd7133f788258104d4d5e4139b1efc72cf
Christian Münch <christian@muench-worms.de>
2022-01-03 14:50:17 +0100

    Add Guzzle to php-scoper whitelist and disable composer patch

--- scoper.inc.php
+++ scoper.inc.php
@@ -14,6 +14,7 @@ return [
         'Composer\\*',
         'Magento\\*',
         'N98\\*',
+        'GuzzleHttp\\*',
         'Symfony\\Polyfill\\*',
         'Symfony\Component\Console\\*'
     ],
