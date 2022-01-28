266dfbc8e73853b4d6b9cccb7e26d3d936cf741e
Martin Århof <martin.aarhof@gmail.com>
2022-01-28 10:45:37 +0100

    Update PreCheckPhp.php
    Fixes #934

--- src/N98/Magento/Command/Installer/SubCommand/PreCheckPhp.php
+++ src/N98/Magento/Command/Installer/SubCommand/PreCheckPhp.php
@@ -50,9 +50,11 @@ class PreCheckPhp extends AbstractSubCommand
      */
     protected function checkXDebug()
     {
-        if (\extension_loaded('xdebug') &&
-            \xdebug_is_enabled() && ini_get('xdebug.max_nesting_level') != -1
-            && \ini_get('xdebug.max_nesting_level') < 200
+        if (\extension_loaded('xdebug') && 
+            function_exists('xdebug_is_enabled') && 
+            \xdebug_is_enabled() && 
+            ini_get('xdebug.max_nesting_level') != -1 && 
+            \ini_get('xdebug.max_nesting_level') < 200
         ) {
             $errorMessage = 'Please change PHP ini setting "xdebug.max_nesting_level". '
                             . 'Please change it to a value >= 200. '
