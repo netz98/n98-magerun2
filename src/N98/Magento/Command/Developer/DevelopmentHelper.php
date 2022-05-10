4a4008350c73874cbdd92c3befcb7ef649515d0c
Christian Münch <christian@muench-worms.de>
2022-05-10 08:16:42 +0200

    Add new functions
    - getStoreManager
    - createProductModel
    - createCustomerModel
    - getScopeConfig
    - getEavAttributeRepository
    - getCmsBlockRepository
    - getCmsPageRepository
    - getDatabaseConnection

--- src/N98/Magento/Command/Developer/DevelopmentHelper.php
+++ src/N98/Magento/Command/Developer/DevelopmentHelper.php
@@ -4,18 +4,41 @@ namespace N98\Magento\Command\Developer;
 
 class DevelopmentHelper
 {
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
     private $di;
 
+    /**
+     * @param \Magento\Framework\ObjectManagerInterface $di
+     */
     public function __construct($di)
     {
         $this->di = $di;
     }
 
+    /**
+     * @param string $sku
+     * @param int $storeId
+     */
     public function debugProductBySku($sku, $storeId = 0)
     {
         return $this->getProductRepository()->get($sku, false, $storeId)->debug();
     }
 
+    /**
+     * @return \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    public function getProductRepository()
+    {
+        return $this->di->get(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+    }
+
+    /**
+     * @param int $id
+     * @param int $storeId
+     * @return mixed
+     */
     public function debugProductById($id, $storeId = 0)
     {
         return $this->getProductRepository()->getById($id, false, $storeId)->debug();
@@ -26,9 +49,46 @@ class DevelopmentHelper
         return $this->getCategoryRepository()->get($id, $storeId)->debug();
     }
 
+    /**
+     * @return \Magento\Catalog\Api\CategoryRepositoryInterface
+     */
+    public function getCategoryRepository()
+    {
+        return $this->di->get(\Magento\Catalog\Api\CategoryRepositoryInterface::class);
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function debugOrderById($id)
+    {
+        return $this->getOrderRepository()->get($id)->debug();
+    }
+
+    /**
+     * @return \Magento\Sales\Api\OrderRepositoryInterface
+     */
+    public function getOrderRepository()
+    {
+        return $this->di->get(\Magento\Sales\Api\OrderRepositoryInterface::class);
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
     public function debugCustomerById($id)
     {
         return $this->getCustomerRepository()->getById($id)->debug();
+    }
+
+    /**
+     * @return \Magento\Customer\Api\CustomerRepositoryInterface
+     */
+    public function getCustomerRepository()
+    {
+        return $this->di->get(\Magento\Customer\Api\CustomerRepositoryInterface::class);
     }
 
     public function debugCustomerByEmail($email, $websiteId = 0)
@@ -36,18 +96,84 @@ class DevelopmentHelper
         return $this->getCustomerRepository()->get($email, $websiteId)->debug();
     }
 
-    public function getProductRepository()
+    /**
+     * @param int $cartId
+     * @return mixed
+     */
+    public function debugCartById($cartId)
     {
-        return $this->di->get('Magento\Catalog\Model\ProductRepository');
+        return $this->getCartRepository()->get($cartId)->debug();
     }
 
-    public function getCategoryRepository()
+    /**
+     * @return \Magento\Quote\Api\CartRepositoryInterface
+     */
+    public function getCartRepository()
     {
-        return $this->di->get('Magento\Catalog\Model\CategoryRepository');
+        return $this->di->get(\Magento\Quote\Api\CartRepositoryInterface::class);
     }
 
-    public function getCustomerRepository()
+    /**
+     * @return \Magento\Store\Model\StoreManagerInterface
+     */
+    public function getStoreManager()
     {
-        return $this->di->get('Magento\Customer\Api\CustomerRepositoryInterface');
+        return $this->di->get(\Magento\Store\Model\StoreManagerInterface::class);
+    }
+
+    /**
+     * @return \Magento\Catalog\Model\Product
+     */
+    public function createProductModel()
+    {
+        return $this->di->get(\Magento\Catalog\Model\ProductFactory::class)->create();
+    }
+
+    /**
+     * @return \Magento\Customer\Model\Customer
+     */
+    public function createCustomerModel()
+    {
+        return $this->di->get(\Magento\Customer\Model\CustomerFactory::class)->create();
+    }
+
+    /**
+     * @return \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    public function getScopeConfig()
+    {
+        return $this->di->get(\Magento\Framework\App\Config\ScopeConfigInterface::class);
+    }
+
+    /**
+     * @return \Magento\Eav\Api\AttributeRepositoryInterface
+     */
+    public function getEavAttributeRepository()
+    {
+        return $this->di->get(\Magento\Eav\Api\AttributeRepositoryInterface::class);
+    }
+
+    /**
+     * @return \Magento\Cms\Api\BlockRepositoryInterface
+     */
+    public function getCmsBlockRepository()
+    {
+        return $this->di->get(\Magento\Cms\Api\BlockRepositoryInterface::class);
+    }
+
+    /**
+     * @return \Magento\Cms\Api\PageRepositoryInterface
+     */
+    public function getCmsPageRepository()
+    {
+        return $this->di->get(\Magento\Cms\Api\PageRepositoryInterface::class);
+    }
+
+    /**
+     * @return \Magento\Framework\DB\Adapter\Pdo\Mysql
+     */
+    public function getDatabaseConnection()
+    {
+        return $this->di->get(\Magento\Framework\App\ResourceConnection::class)->getConnection();
     }
 }
