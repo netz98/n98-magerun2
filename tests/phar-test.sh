454b3b57591bde393cf1334e433faa33d7cbfdfc
Christian Münch <christian@muench-worms.de>
2023-01-24 16:06:50 +0100

    Add phar test for config:data:indexer command

--- tests/phar-test.sh
+++ tests/phar-test.sh
@@ -132,6 +132,8 @@ function test_magerun_commands() {
 	assert_command_contains "config:data:acl" "ACL Tree"
 	#  config:data:di
 	assert_command_contains "config:data:di" "DateTimeInterface"
+	#  config:data:indexer
+  	assert_command_contains "config:data:indexer" "catalog_product_flat"
 	#  config:env:create
 	#  config:env:set
 	assert_command_contains "config:env:set magerun.example foo" "Config magerun.example successfully set to foo"
