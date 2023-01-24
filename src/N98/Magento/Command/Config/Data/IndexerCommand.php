a8ca94216ed7f93c0ab5196dc05d19939a39885a
Christian Münch <christian@muench-worms.de>
2023-01-24 16:30:56 +0100

    Add tree renderer

--- src/N98/Magento/Command/Config/Data/IndexerCommand.php
+++ src/N98/Magento/Command/Config/Data/IndexerCommand.php
@@ -4,6 +4,7 @@ namespace N98\Magento\Command\Config\Data;
 
 use N98\Magento\Command\AbstractMagentoCommand;
 use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
+use PBergman\Console\Helper\TreeHelper;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -27,6 +28,7 @@ class IndexerCommand extends AbstractMagentoCommand
                 'Config scope (global, adminhtml, frontend, graphql, webapi_rest, webapi_soap, ...)',
                 'global'
             )
+            ->addOption('tree', 't', InputOption::VALUE_NONE, 'Show data as tree')
             ->addOption(
                 'format',
                 null,
@@ -54,6 +56,58 @@ class IndexerCommand extends AbstractMagentoCommand
     {
         $data = $this->indexConfigReader->read();
 
+        if ($input->getOption('tree')) {
+            $this->renderAsTree($data, $output);
+        } else {
+            $this->renderAsTable($data, $output, $input);
+        }
+
+        return Command::SUCCESS;
+    }
+
+    protected function renderAsTree(array $data, OutputInterface $output)
+    {
+        $tree = new TreeHelper();
+        $tree->setTitle('Indexer Data Tree');
+
+        foreach ($data as $row) {
+            $node = $tree->newNode('<info>' . $row['title'] . '</info>');
+
+            $actionClassNode = $node->newNode('indexer_id:');
+            $actionClassNode->addValue('<comment>' . $row['indexer_id'] . '</comment>');
+
+            $viewIdNode = $node->newNode('view_id:');
+            $viewIdNode->addValue('<comment>' . $row['view_id'] . '</comment>');
+
+            if (count($row['dependencies']) > 0) {
+                $dependenciesNode = $node->newNode('dependencies:');
+                foreach ($row['dependencies'] as $dependency) {#
+                    $dependenciesNode->addValue('<comment>' . $dependency . '</comment>');
+                }
+            }
+
+            if (!empty($row['action_class'])) {
+                $actionClassNode = $node->newNode('action_class:');
+                $actionClassNode->addValue('<comment>' . $row['action_class'] . '</comment>');
+            }
+
+            if (!empty($row['shared_index'])) {
+                $sharedIndexNode = $node->newNode('shared_index:');
+                $sharedIndexNode->addValue('<comment>' . $row['shared_index'] . '</comment>');
+            }
+        }
+
+        $tree->printTree($output);
+    }
+
+    /**
+     * @param array $data
+     * @param OutputInterface $output
+     * @param InputInterface $input
+     * @return void
+     */
+    protected function renderAsTable(array $data, OutputInterface $output, InputInterface $input): void
+    {
         $table = [];
 
         foreach ($data as $indexer) {
@@ -63,13 +117,12 @@ class IndexerCommand extends AbstractMagentoCommand
                 'shared_index' => $indexer['shared_index'],
                 'title' => $indexer['title'],
                 'dependencies' => implode(',', $indexer['dependencies']),
+                'action_class' => $indexer['action_class'],
             ];
         }
 
         $this->getHelper('table')
-            ->setHeaders(['indexer_id', 'view_id', 'shared_index', 'title', 'dependencies'])
+            ->setHeaders(['indexer_id', 'view_id', 'shared_index', 'title', 'dependencies', 'action_class'])
             ->renderByFormat($output, $table, $input->getOption('format'));
-
-        return Command::SUCCESS;
     }
 }
