df4114ffe8239e96eaea5a63e1aa53c70b63ed5b
Christian Münch <christian@muench-worms.de>
2023-04-28 10:22:18 +0200

    Automatic php version and framework dependency detection
    Introduces a abstraction to read data of composer.lock.
    Use current php version and actual installed version of
    magento/framework as version of dependencies for new
    generated module.

---
+++ tests/N98/Util/ComposerLockTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @copyright Copyright (c) netz98 GmbH (https://www.netz98.de)
+ *
+ * @see PROJECT_LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace N98\Util;
+
+class ComposerLockTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function itShouldBeIteratable()
+    {
+        $lock = new ComposerLock(__DIR__ . '/_files/sample-project/composer');
+        $this->assertIsIterable($lock);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function itShouldReturnPackages()
+    {
+        $lock = new ComposerLock(__DIR__ . '/_files/sample-project/composer');
+        $this->assertIsArray($lock->getPackages());
+
+        $this->assertEquals('1.1.1', $lock->getPackageByName('psr/container')->version);
+    }
+
+}
