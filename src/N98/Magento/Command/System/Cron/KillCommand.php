7bf8bd16622ea63a987be1f2620c21c7139c0b70
Christian Münch <christian@muench-worms.de>
2023-06-20 11:24:11 +0200

    Import the correct Command class

--- src/N98/Magento/Command/System/Cron/KillCommand.php
+++ src/N98/Magento/Command/System/Cron/KillCommand.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
 
 namespace N98\Magento\Command\System\Cron;
 
-use _PHPStan_a3459023a\Symfony\Component\Console\Command\Command;
 use Magento\Cron\Model\Schedule;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
