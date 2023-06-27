17c90fa9eb3c6c3eb8a11c203dbc48cb0763f133
Christian Münch <christian@muench-worms.de>
2023-06-27 09:12:05 +0200

    Add additional help text and refactor code

--- src/N98/Magento/Command/Sales/SequenceAddCommand.php
+++ src/N98/Magento/Command/Sales/SequenceAddCommand.php
@@ -4,6 +4,9 @@ namespace N98\Magento\Command\Sales;
 
 use Magento\Framework\Event;
 use Magento\Framework\Event\Observer;
+use Magento\SalesSequence\Observer\SequenceCreatorObserver;
+use Magento\Store\Api\StoreInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use N98\Magento\Command\AbstractMagentoCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -15,9 +18,26 @@ class SequenceAddCommand extends AbstractMagentoCommand
 
     protected function configure()
     {
+        $help = <<<'HELP'
+                The 'sales:sequence:add' command adds sequence tables and metadata 
+                for a specified store or for all stores if no store is specifically mentioned.
+                The store argument refers to the specific store for which the sequence tables 
+                and metadata are to be added. This can be specified by either the store code or the store ID. 
+                If no store is specified, the command will apply to all stores.
+                
+                <comment>Examples</comment>:
+                
+                - Adds sequence tables and metadata for the store with ID 5.
+                  <info>sales:sequence:add 5</info>         
+                
+                - Adds sequence tables and metadata for the store with code 'store_en'. 
+                  <info>sales:sequence:add 'store_en'</info>
+                HELP;
+
         $this->setName('sales:sequence:add')
             ->setDescription('Add the sequence tables and metadata for given store or all stores')
-            ->addArgument('store', InputArgument::OPTIONAL, 'Store code');
+            ->setHelp($help)
+            ->addArgument('store', InputArgument::OPTIONAL, 'The store code or ID');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -31,17 +51,24 @@ class SequenceAddCommand extends AbstractMagentoCommand
 
         $di = $this->getObjectManager();
 
-        $storeManager = $di->get('Magento\Store\Model\StoreManagerInterface');
-        $sequenceCreator = $di->get('Magento\SalesSequence\Observer\SequenceCreatorObserver');
+        $storeManager = $di->get(StoreManagerInterface::class);
+        $sequenceCreator = $di->get(SequenceCreatorObserver::class);
 
         $stores = [$storeManager->getStore($storeCode)];
         if (!$storeCode) {
             $stores = $storeManager->getStores();
         }
 
-        /** @var \Magento\Store\Api\StoreInterface $store */
+        /** @var StoreInterface $store */
         foreach ($stores as $store) {
-            $output->writeln(sprintf('<info>Updating sequence for store <comment>%s</comment> (<comment>%s #%d</comment>)</info>', $store->getName(), $store->getCode(), $store->getId()));
+            $output->writeln(
+                sprintf(
+                    '<info>Updating sequence for store <comment>%s</comment> (<comment>%s #%d</comment>)</info>',
+                    $store->getName(),
+                    $store->getCode(),
+                    $store->getId()
+                )
+            );
 
             $event = new Event(['store' => $store]);
             $event->setName('store_add');
