c05862c94a154e92edeaf035eb985bddfa72be64
Christian Münch <christian@muench-worms.de>
2023-12-26 17:08:27 +0100

    WIP try to use the existing search class of the backend module

--- src/N98/Magento/Command/Config/SearchCommand.php
+++ src/N98/Magento/Command/Config/SearchCommand.php
@@ -10,21 +10,24 @@ declare(strict_types=1);
 namespace N98\Magento\Command\Config;
 
 use Magento\Backend\Model\Search\Config\Result\Builder;
+use Magento\Backend\Model\Search\Config as ConfigSeaerch;
 use Magento\Config\Model\Config\Structure as ConfigStructure;
-use Magento\Config\Model\Config\Structure\Element\AbstractComposite;
-use Magento\Config\Model\Config\Structure\Element\Iterator as ElementIterator;
+use Magento\Config\Model\Config\Structure\Data as ConfigStructureData;
 use Magento\Framework\App\Area;
 use Magento\Framework\App\AreaList;
 use Magento\Framework\App\State;
 use Magento\Framework\ObjectManager\ConfigLoaderInterface;
 use N98\Magento\Command\AbstractMagentoCommand;
+use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SearchCommand extends AbstractMagentoCommand
 {
     private ConfigStructure $configStructure;
+    private ConfigStructureData $configStructureData;
     private Builder $resultBuilder;
 
     protected function configure()
@@ -37,7 +40,14 @@ class SearchCommand extends AbstractMagentoCommand
                 Searches the merged system.xml configuration tree <labels/> and <comments/> for the indicated text.
 EOT
             )
+            ->addOption(
+                'format',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
+            )
             ->addArgument('text', InputArgument::REQUIRED, 'The text to search for');
+
         parent::configure();
     }
 
@@ -55,41 +65,85 @@ EOT
 
         $this->setAdminArea();
 
+        $appState = $this->getObjectManager()->get(State::class);
+        $appState->emulateAreaCode('adminhtml', function () use ($input, $output) {
+            $results = $this->getObjectManager()->create(ConfigSeaerch::class)
+                ->setStart(0)
+                ->setLimit(100)
+                ->setQuery($input->getArgument('text'))
+                ->load()
+                ->getResults();
+
+            var_dump($results);
+            die;
+        });
+
         $this->configStructure = $this->getObjectManager()->create(ConfigStructure::class);
+        $tabs = $this->configStructure->getTabs();
+
+        die;
+
+        if ($tabs instanceof \Iterator) {
+            $tabs = iterator_to_array($tabs);
+        }
+        var_dump($tabs);
+        die;
+        $this->configStructureData = $this->getObjectManager()->create(ConfigStructureData::class);
         $this->resultBuilder = $this->getObjectManager()->create(Builder::class);
 
-        $this->findInStructure(
-            $this->configStructure->getTabs(),
-            $input->getArgument('text')
-        );
-        var_dump($this->resultBuilder->getAll());
+        $configData = $this->configStructureData->get();
+        if (isset($configData['sections'])) {
+            $this->findInStructure(
+                $configData['sections'],
+                $input->getArgument('text')
+            );
+        }
+
+        $results = $this->resultBuilder->getAll();
+
+        if (empty($results)) {
+            $output->writeln('<info>No results found.</info>');
+            return self::SUCCESS;
+        }
+
+        $tableData = [];
+        foreach ($results as $result) {
+            $tableData[] = [
+                $result['name'],
+                $result['description'],
+            ];
+        }
+
+        $this->getHelper('table')
+            ->setHeaders(['Name', 'Description'])
+            ->renderByFormat($output, $tableData, $input->getOption('format'));
 
         return self::SUCCESS;
     }
 
     /**
-     * Copy of core logic from \Magento\Backend\Model\Search\Config::findInStructure
-     *
-     * @param ElementIterator $structureElementIterator
+     * @param array $elements
      * @param string $searchTerm
-     * @param string $pathLabel
-     * @return void
-     * @see \Magento\Backend\Model\Search\Config::findInStructure
-     * @SuppressWarnings(PHPMD.LongVariable)
      */
-    private function findInStructure(ElementIterator $structureElementIterator, $searchTerm, $pathLabel = '')
+    private function findInStructure($elements, $searchTerm)
     {
         if (empty($searchTerm)) {
             return;
         }
 
-        foreach ($structureElementIterator as $structureElement) {
-            if (mb_stripos((string)$structureElement->getLabel(), $searchTerm) !== false) {
-                $this->resultBuilder->add($structureElement, $pathLabel);
+        foreach ($elements as $elementData) {
+            // search in label
+            if (isset($elementData['label']) && mb_stripos((string)$elementData['label'], $searchTerm) !== false) {
+                // if element has a path, add it to the result
+                if (isset($elementData['path'])) {
+                    $element = $this->configStructure->getElementByConfigPath($elementData['path']);
+                    $this->resultBuilder->add($element, $elementData['path']);
+                }
             }
-            $elementPathLabel = $pathLabel . ' / ' . $structureElement->getLabel();
-            if ($structureElement instanceof AbstractComposite && $structureElement->hasChildren()) {
-                $this->findInStructure($structureElement->getChildren(), $searchTerm, $elementPathLabel);
+
+            // If the element has children (like groups), recurse
+            if (isset($elementData['children'])) {
+                $this->findInStructure($elementData['children'], $searchTerm);
             }
         }
     }
@@ -111,6 +165,5 @@ EOT
         $areaList->getArea(Area::AREA_ADMINHTML)
             ->load(Area::PART_CONFIG)
             ->load(Area::PART_TRANSLATE);
-
     }
 }
