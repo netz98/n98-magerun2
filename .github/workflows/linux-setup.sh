ebe9bcca12e443ac862c075273827c8033088a81
Christian Münch <christian@muench.dev>
2025-04-28 17:54:35 +0200

    build: fix issue after change of ubuntu distro
    Signed-off-by: Christian Münch <christian@muench.dev>

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -9,7 +9,7 @@ set -x
 # bats (for testing)
 git clone --branch v1.2.1 https://github.com/bats-core/bats-core.git /tmp/bats-core && pushd /tmp/bats-core >/dev/null && sudo ./install.sh /usr/local
 
-sudo apt-get update
+sudo apt-get update --allow-releaseinfo-change
 sudo apt-get -y install zstd lz4
 
 # Show info to simplify debugging
