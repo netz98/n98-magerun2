f53da50c59f77e69516c390826be38c0505118fb
Christian Münch <christian@muench.dev>
2025-05-29 12:27:02 +0200

    chore: update linux setup script and workflow to include mydumper and streamline installation
    Signed-off-by: Christian Münch <christian@muench.dev>

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -10,7 +10,7 @@ set -x
 git clone --branch v1.2.1 https://github.com/bats-core/bats-core.git /tmp/bats-core && pushd /tmp/bats-core >/dev/null && sudo ./install.sh /usr/local
 
 sudo apt-get update --allow-releaseinfo-change
-sudo apt-get -y install zstd lz4
+sudo apt-get -y install zstd lz4 sendmail mydumper
 
 # Show info to simplify debugging
 lsb_release -a
