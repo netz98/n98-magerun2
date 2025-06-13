8d880447ce588d4db846c2d526421912b90da655
Christian Münch <christian@muench.dev>
2025-06-13 15:21:10 +0200

    chore: update application version to 9.0.0-alpha1

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -47,7 +47,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '8.1.1';
+    const APP_VERSION = '9.0.0-alpha1';
 
     /**
      * @var int
