b84b4dbb7186d508f969d2efdea52ab359249a0a
Christian Münch <christian@muench.dev>
2025-06-23 10:56:48 +0200

    style(HomepageFeatures): standardize class attribute quotes in JSX
    Signed-off-by: Christian Münch <christian@muench.dev>

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -52,15 +52,15 @@ const FeatureList = [
 export default function HomepageFeatures () {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="row">
+      <div className='container'>
+        <div className='row'>
           {FeatureList.map((props, idx) => (
             // <Feature key={idx} {...props} />
             <div key={idx} className={clsx('col col--4')}>
-              <div className="text--center">
-                <props.Svg className={styles.featureSvg} role="img" />
+              <div className='text--center'>
+                <props.Svg className={styles.featureSvg} role='img' />
               </div>
-              <div className="text--center padding-horiz--md">
+              <div className='text--center padding-horiz--md'>
                 <h3>{props.title}</h3>
                 <p>{props.description}</p>
               </div>
