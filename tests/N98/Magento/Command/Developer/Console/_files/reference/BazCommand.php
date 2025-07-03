9eb1e3d2a2d2892bdd5bdd53a535958e77b71503
Christian Münch <christian@muench.dev>
2025-07-03 17:07:24 +0200

    chore: normalize file headers in source files

--- tests/N98/Magento/Command/Developer/Console/_files/reference/BazCommand.php
+++ tests/N98/Magento/Command/Developer/Console/_files/reference/BazCommand.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * This file is part of the n98-magerun2 project.
- *
- * For the full copyright and license information, please view the MIT-LICENSE.txt
- * file that was distributed with this source code.
- */
 
 namespace N98\Dummy\Console\Command\Foo\Bar;
 
@@ -28,7 +22,7 @@ class BazCommand extends Command
      * @param OutputInterface $output An OutputInterface instance
      * @return null|int null or 0 if everything went fine, or an error code
      */
-    public function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('BazCommand');
     }
