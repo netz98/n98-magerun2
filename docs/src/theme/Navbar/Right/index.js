a9e83fa2a64de13ed7d983bf7f03edf12ef6b934
Christian Münch <christian@muench.dev>
2025-07-04 16:25:34 +0200

    style: improve formatting and consistency in NavbarRightWrapper component

--- docs/src/theme/Navbar/Right/index.js
+++ docs/src/theme/Navbar/Right/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import NavbarRight from '@theme-original/Navbar/Right';
-import './styles.css';
+import React from 'react'
+import NavbarRight from '@theme-original/Navbar/Right'
+import './styles.css'
 
-export default function NavbarRightWrapper(props) {
+export default function NavbarRightWrapper (props) {
   return (
-    <div className="navbar__right-wrapper">
+    <div className='navbar__right-wrapper'>
       <NavbarRight {...props} />
     </div>
-  );
+  )
 }
