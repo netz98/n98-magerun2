c06e3578a0873e210ac509c67f0146aea0f2c8e1
Christian Münch <christian@muench.dev>
2026-02-10 07:27:35 +0100

    test: mock integration flush command environment

--- tests/N98/Magento/Command/Test/Integration/FlushCommandTest.php
+++ tests/N98/Magento/Command/Test/Integration/FlushCommandTest.php
@@ -8,7 +8,11 @@
 
 namespace N98\Magento\Command\Test\Integration;
 
-use N98\Magento\Command\TestCase;
+use N98\Magento\Application;
+use N98\Util\Filesystem;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Tester\CommandTester;
 
 /**
  * Class FlushCommandTest
@@ -16,13 +20,80 @@ use N98\Magento\Command\TestCase;
  */
 class FlushCommandTest extends TestCase
 {
-    public function testExecuteWithEmptySandboxDir()
+    /**
+     * @var string[]
+     */
+    private $tempDirectories = [];
+
+    protected function tearDown(): void
     {
-        $this->assertDisplayContains('test:integration:flush', 'No sandbox directories found');
+        $filesystem = new Filesystem();
+
+        foreach ($this->tempDirectories as $directory) {
+            if (is_dir($directory)) {
+                $filesystem->recursiveRemoveDirectory($directory);
+            }
+        }
+
+        $this->tempDirectories = [];
     }
 
-    public function testExecuteWithForceOption()
+    public function testExecuteWithoutMagentoRoot()
     {
-        $this->assertDisplayContains('test:integration:flush --force', 'No sandbox directories found');
+        $tester = $this->createCommandTester('');
+        $tester->execute(['command' => 'test:integration:flush']);
+
+        $this->assertStringContainsString('Could not determine Magento root directory', $tester->getDisplay());
+    }
+
+    public function testExecuteWithoutIntegrationTests()
+    {
+        $magentoRoot = $this->createTempDirectory();
+
+        $tester = $this->createCommandTester($magentoRoot);
+        $tester->execute(['command' => 'test:integration:flush']);
+
+        $this->assertStringContainsString('No integration tests directory found', $tester->getDisplay());
+    }
+
+    public function testExecuteWithEmptySandboxDir()
+    {
+        $magentoRoot = $this->createTempDirectory();
+        $tempPath = $magentoRoot . '/dev/tests/integration/tmp';
+        mkdir($tempPath, 0777, true);
+
+        $tester = $this->createCommandTester($magentoRoot);
+        $tester->execute(['command' => 'test:integration:flush']);
+
+        $this->assertStringContainsString('No sandbox directories found', $tester->getDisplay());
+    }
+
+    private function createTempDirectory(): string
+    {
+        $directory = rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR)
+            . DIRECTORY_SEPARATOR
+            . 'n98-magerun2-test-'
+            . uniqid('', true);
+
+        mkdir($directory, 0777, true);
+
+        $this->tempDirectories[] = $directory;
+
+        return $directory;
+    }
+
+    private function createCommandTester(string $magentoRoot): CommandTester
+    {
+        $application = $this->createMock(Application::class);
+        $application->method('detectMagento');
+        $application->method('getMagentoRootFolder')->willReturn($magentoRoot);
+        $application->method('isMagentoEnterprise')->willReturn(false);
+        $application->method('getMagentoMajorVersion')->willReturn(2);
+
+        $command = new FlushCommand();
+        $command->setApplication($application);
+        $command->setHelperSet(new HelperSet());
+
+        return new CommandTester($command);
     }
 }
